444c29b87b9f421d20cd144265d9e9dbb652a576
Bekione <kinber024@gmail.com>
2023-07-04 10:15:35 +0300

    Fixed import error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react
+import {useState} from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Header from './Components/Header'
 import SideBar from './Components/SideBar'
