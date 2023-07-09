6eb07342717c6ebb54ee46999437a66c1a24c63d
Bekione <kinber024@gmail.com>
2023-07-09 16:21:50 +0300

    Fixed home page slider

--- src/Pages/Home/ImageSlider.js
+++ src/Pages/Home/ImageSlider.js
@@ -1,31 +1,29 @@
 import { Swiper, SwiperSlide } from 'swiper/react';// Import Swiper React components
-import { Navigation, Pagination, Grid, Autoplay } from 'swiper/modules';// import required modules
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';// import required modules
+// import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+// import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import 'swiper/css';
-import 'swiper/css/grid';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 function ImageSlider({isDesktop}) {
-    const images = [
-        require('../Assets/desk_snap_health.png'),
-        require('../Assets/desk_snap_diet.png'),
-        require('../Assets/desk_snap_zodiac.png'),
-        require('../Assets/desk_snap_health_2.png'),
-        require('../Assets/desk_snap_diet_2.png'),
-        require('../Assets/desk_snap_zodiac_2.png'),
-        require('../Assets/desk_snap_health_2.png'),
-        require('../Assets/desk_snap_health.png'),
-        require('../Assets/desk_snap_zodiac.png'),
-        require('../Assets/desk_snap_diet_2.png')
-    ];
+  const images = [
+      require('../Assets/desk_snap_health.png'),
+      require('../Assets/desk_snap_diet.png'),
+      require('../Assets/desk_snap_zodiac.png'),
+      require('../Assets/desk_snap_health_2.png'),
+      require('../Assets/desk_snap_diet_2.png'),
+      require('../Assets/desk_snap_zodiac_2.png'),
+      require('../Assets/desk_snap_health_2.png'),
+      require('../Assets/desk_snap_health.png'),
+      require('../Assets/desk_snap_zodiac.png'),
+      require('../Assets/desk_snap_diet_2.png')
+  ];
 
   return (
     <>
-      <div className='navigators'>
-        <button className='swiper-button-prev btn' title='Prev'><ArrowBackIosNewIcon /></button>
-        <button className='swiper-button-next btn' title='Next'><ArrowForwardIosIcon /></button>
-      </div>
+      <div className='swiper-button-prev'></div>
+      <div className='swiper-button-next'></div>
       <Swiper
         slidesPerView={1}
         spaceBetween={30}
@@ -41,7 +39,7 @@ function ImageSlider({isDesktop}) {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev'
         }}
-        modules={[Grid, Autoplay, Pagination, Navigation]}
+        modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
         {
