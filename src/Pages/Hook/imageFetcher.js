21d836d39f6926b3039cf526baa4efae25ce92f3
Bekione <kinber024@gmail.com>
2023-07-10 13:53:03 +0300

    Added image fetcher methods that fetch from api

---
+++ src/Pages/Hook/imageFetcher.js
@@ -0,0 +1,29 @@
+import {useEffect, useState} from 'react'
+import api from '../../api'
+
+export default function ImageFetcher(path){
+    const [images, setImages] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const imageResponse = await api.get(`/api/asset/${path}`)
+                setImages(imageResponse.data)
+            }
+            catch(error){
+                console.error(error)
+            }
+        }
+ 
+        fetchData()
+    }, [])
+
+    return images
+}
+
+export function getImagePath(images, name) {
+    const image = images.find(image => image.name === name);
+
+    if (image) {
+      return image.path;
+    }
+}
