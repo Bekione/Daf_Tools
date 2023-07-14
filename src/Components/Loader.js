c4f1399fbd3d3c6f02bc100a41481a6018d53163
Bekione <kinber024@gmail.com>
2023-07-15 00:27:20 +0300

    Added page loading component

---
+++ src/Components/Loader.js
@@ -0,0 +1,11 @@
+import './loader.css'
+
+const Loader = () => {
+    return(
+        <div className='page_body daf_loader'>
+            <div className="loader">Loading...</div>
+        </div>
+    )
+}
+
+export default Loader
