1ee6bd77b1d54fab01f81710eb5d9032d825944d
Bekione <kinber024@gmail.com>
2023-07-15 04:15:02 +0300

    Added useRef hook, new JSX reutrn and update handleMouseMove

--- src/Pages/Home/HoverableImage.js
+++ src/Pages/Home/HoverableImage.js
@@ -1,41 +1,47 @@
-import { useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import './style.css';
 
-const HoverableImage = ({imgPath}) => {
-    const [isHovering, setIsHovering] = useState(false);
-    const [rotateX, setRotateX] = useState(0);
-    const [rotateY, setRotateY] = useState(0);
-    const [boxShadow, setBoxShadow] = useState('none');
-  
+const HoverableImage = ({ src, alt }) => {
+  const imageRef = useRef(null)
+  const [rotateX, setRotateX] = useState(0)
+  const [rotateY, setRotateY] = useState(0)
+
+  useEffect(() => {
     const handleMouseMove = (e) => {
-        const containerWidth = e.currentTarget.offsetWidth;
-        const containerHeight = e.currentTarget.offsetHeight;
-        const mouseX = e.pageX - e.currentTarget.offsetLeft - containerWidth / 2;
-        const mouseY = e.pageY - e.currentTarget.offsetTop - containerHeight / 2;
-        const newRotateX = isHovering ? -mouseY / containerHeight * 20 : 0;
-        const newRotateY = isHovering ? mouseX / containerWidth * 20 : 0;
-        setRotateX(newRotateX);
-        setRotateY(newRotateY);
+      const { top, left, width, height } = imageRef.current.getBoundingClientRect()
+      const x = e.clientX - (left + width / 2)
+      const y = e.clientY - (top + height / 2)
 
-        const shadowX = (mouseX / containerWidth * 10).toFixed(2);
-        const shadowY = (-mouseY / containerHeight * 10).toFixed(2);
-        setBoxShadow(`${shadowX}px ${shadowY}px 10px rgba(0, 0, 0, 0.5)`);
+      setRotateX(-y / 20)
+      setRotateY(x / 10)
     };
 
-    const handleMouseEnter = () => {
-        setIsHovering(true);
-    };
-        
     const handleMouseLeave = () => {
-        setIsHovering(false);
-        setRotateX(0);
-        setRotateY(0);
-        setBoxShadow('none');
+      setRotateX(0)
+      setRotateY(0)
     }
-  return (
-    <div className='about_mobile_img' onMouseMove={handleMouseMove} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
-        <img src={imgPath} alt='' className='' style={{ transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`}}/>
-    </div>
-  )
-}
 
-export default HoverableImage
+    const img = imageRef.current;
+    img.addEventListener('mousemove', handleMouseMove)
+    img.addEventListener('mouseleave', handleMouseLeave)
+
+    return () => {
+      img.removeEventListener('mousemove', handleMouseMove)
+      img.removeEventListener('mouseleave', handleMouseLeave)
+    }
+  }, [])
+
+  return (
+    <div className="hoverable_image_perspective">
+      <img
+        ref={imageRef}
+        src={src}
+        alt={alt}
+        style={{ transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)` }}
+        className='hoverable_image'
+      />
+    </div>
+  );
+};
+
+export default HoverableImage;
