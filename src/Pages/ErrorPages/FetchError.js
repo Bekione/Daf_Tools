fe79bb166962d6e6cea7f7334bde09d041ed02c6
Bekione <kinber024@gmail.com>
2023-07-16 23:26:45 +0300

    Added a wrapper container

--- src/Pages/ErrorPages/FetchError.js
+++ src/Pages/ErrorPages/FetchError.js
@@ -2,12 +2,14 @@ import './style.css'
 
 const FetchError = ({errorMsg}) => {
     return (
-        <div className='daf_error_message_wrapper'>
-            <h2 className='error_message_title'>Error Occured {`:(`}</h2>
-            <div className='error_message_description'>
-                <p className='error_message_code'>{errorMsg}</p>
-                <p>Failed to load resources</p>
-                <p>Please check your internet connection</p>
+        <div className='page_body'>
+            <div className='daf_error_message_wrapper'>
+                <h2 className='error_message_title'>Error Occured {`:(`}</h2>
+                <div className='error_message_description'>
+                    <p className='error_message_code'>{errorMsg}</p>
+                    <p>Failed to load resources</p>
+                    <p>Please check your internet connection</p>
+                </div>
             </div>
         </div>
     )
