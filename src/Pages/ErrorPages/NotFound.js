6a746fbcdde72b9dcefd0c2b58fe20a194e37d7c
Bekione <kinber024@gmail.com>
2023-07-18 13:30:20 +0300

    Added 404 page contents

--- src/Pages/ErrorPages/NotFound.js
+++ src/Pages/ErrorPages/NotFound.js
@@ -1,9 +1,23 @@
+import {Link} from 'react-router-dom'
+import bg from '../Assets/404.jpg'
+import './style.css'
+
 const NotFound = () => {
-    return (
-        <div className='page_body'>
-            <h2>Page not found</h2>
+
+  return (
+    <div className='page_body error'>
+        <div className="notfound" style={{backgroundImage: `url(${bg})`}}>
+            <div className="notfound-404">
+                <h1>404</h1>
+                <p>we are sorry, but the page you requested was not found</p>
+            </div>
+            <div className='buttons'>
+                <Link to="/" className="home-btn">Go Home</Link>
+                <Link to="https://www.github.com/bekione/daftools.git" target="_blank" className="sourcecode-btn">Get source code</Link>
+            </div>
         </div>
-    )
+    </div>
+  )
 }
 
 export default NotFound
